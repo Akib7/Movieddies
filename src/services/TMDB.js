d56ba23c845599e10ddd3d94761f59ce7ba2f197
Akib Ahmed <akibahmed7360@gmail.com>
2022-09-28 21:53:32 +0600

    started working with the TMDB api features

--- src/services/TMDB.js
+++ src/services/TMDB.js
@@ -1,18 +1,18 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-const tmdbApiKey = process.env.TMDB_key;
+const tmdbApiKey = process.env.REACT_APP_TMDB_KEY;
 const page = 1;
 // /movie/popular?api_key=<<api_key>>&language=en-US&page=1
 
 export const tmdbApi = createApi({
   reducerPath: "tmdbApi",
-  basedQuery: fetchBaseQuery({
+  baseQuery: fetchBaseQuery({
     baseUrl: "https://api.themoviedb.org/3",
-    endpoints: (builder) => ({
-      //* Get Movies by [type]
-      getMovies: builder.query({
-        query: () => `movie/popular?page=${page}&api_key=${tmdbApiKey}`,
-      }),
+  }),
+  endpoints: (builder) => ({
+    //* Get Movies by [type]
+    getMovies: builder.query({
+      query: () => `movie/popular?page=${page}&api_key=${tmdbApiKey}`,
     }),
   }),
 });
