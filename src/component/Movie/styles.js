cf928f991f7036787be25598bd1cbb93997c4cf9
Akib Ahmed <akibahmed7360@gmail.com>
2022-10-01 00:18:55 +0600

    fetched data from api and integrated the Movie component with rating, image, and title

--- src/component/Movie/styles.js
+++ src/component/Movie/styles.js
@@ -7,12 +7,32 @@ export default makeStyles((theme) => ({
   title: {
     color: theme.palette.text.primary,
     textOverflow: "ellipsis",
-    width: "200px",
-    whiteSpace: "nowrap",
+    width: "230px",
     overflow: "hidden",
+    whiteSpace: "nowrap",
     marginTop: "10px",
     marginBottom: 0,
     textAlign: "center",
+  },
+  links: {
+    alignItems: "center",
+    fontWeight: "bolder",
+    textDecoration: "none",
+    [theme.breakpoints.up("xs")]: {
+      display: "flex",
+      flexDirection: "column",
+    },
+    "&:hover": {
+      cursor: "pointer",
+    },
+  },
+  image: {
+    borderRadius: "20px",
+    height: "300px",
+    marginBottom: "10px",
+    "&:hover": {
+      transform: "scale(1.05)",
+    },
   },
 }));
 
--- src/component/MovieList/styles.js
+++ src/component/MovieList/styles.js
@@ -3,9 +3,9 @@ import { makeStyles } from "@mui/styles";
 export default makeStyles((theme) => ({
   moviesContainer: {
     display: "flex",
-    flexWrap: "wrap",
     justifyContent: "space-between",
     overflow: "auto",
+    flexWrap: "wrap",
     [theme.breakpoints.down("sm")]: {
       justifyContent: "center",
     },
