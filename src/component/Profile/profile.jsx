96f3ddb1d35d0a184f039d805a695ec231c196e4
Akib Ahmed <akibahmed7360@gmail.com>
2022-10-09 19:48:45 +0600

    successfully integrated the authentication

--- src/component/Profile/profile.jsx
+++ src/component/Profile/profile.jsx
@@ -1,8 +1,14 @@
 import React from "react";
+import { useSelector } from "react-redux";
+
+import { userSelector } from "../../features/auth";
+import "./profile.styles.scss";
 
 const Profile = () => {
   console.log("Profile");
-  return <div>Profile</div>;
+
+  const { user } = useSelector(userSelector);
+  return <div className="profile">{`UserID - ${user.id}`}</div>;
 };
 
 export default Profile;
