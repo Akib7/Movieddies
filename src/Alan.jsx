27dddded08fd966469e621928a6ddd836b5d7f6b
Akib Ahmed <akibahmed7360@gmail.com>
2022-10-21 23:30:55 +0600

    implemented the search with different genres and categories with alan AI, and also added some Ester eggs

--- src/Alan.jsx
+++ src/Alan.jsx
@@ -1,16 +1,39 @@
 import React, { useEffect, useContext } from "react";
 import alanBtn from "@alan-ai/alan-sdk-web";
 import { ColorModeContext } from "./utils/ToggleColorMode";
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import {
+  selectGenreOrCategory,
+  searchMovie,
+} from "./features/currentGenreOrCategory";
 import { fetchToken } from "./utils";
 
 const useAlan = () => {
   const { setMode } = useContext(ColorModeContext);
-
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
   useEffect(() => {
     alanBtn({
       key: "c27f89ed8a84c8dd318014d20c489a8a2e956eca572e1d8b807a3e2338fdd0dc/stage",
-      onCommand: ({ command, mode }) => {
-        if (command === "changeMode") {
+      onCommand: ({ command, mode, genres, genreOrCategory, query }) => {
+        if (command === "chooseGenre") {
+          const foundGenre = genres.find(
+            (g) => g.name.toLowerCase() === genreOrCategory.toLowerCase()
+          );
+
+          if (foundGenre) {
+            navigate("/");
+            dispatch(selectGenreOrCategory(foundGenre.id));
+          } else {
+            const category = genreOrCategory.startsWith("top")
+              ? "top_rated"
+              : genreOrCategory;
+
+            navigate("/");
+            dispatch(selectGenreOrCategory(category));
+          }
+        } else if (command === "changeMode") {
           if (mode === "light") {
             setMode("light");
           } else {
@@ -22,6 +45,8 @@ const useAlan = () => {
           localStorage.clear();
 
           window.location.href = "/";
+        } else if (command === "search") {
+          dispatch(searchMovie(query));
         }
       },
     });
